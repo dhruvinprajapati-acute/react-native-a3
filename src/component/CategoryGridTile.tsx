b146359edbd11976eb9acd68357a035989ea9632
Dhruvin Prajapati <dhruvin.prajapati@acuteinformatics.in>
2024-08-29 13:25:01 +0530

    style the card

--- src/component/CategoryGridTile.tsx
+++ src/component/CategoryGridTile.tsx
@@ -1,18 +1,46 @@
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function CategoryGridTile({title, color} : any): React.JSX.Element{
     return(
-        <View>
-            <Pressable>
-                <View>
-                    <Text>
-                        {title}, {color}
+        <View style={styles.gridItem}>
+            <Pressable style={styles.button}>
+                <View style={styles.InnerContainer}>
+                    <Text style={styles.title}>
+                        {title}
                     </Text>
                 </View>
             </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    gridItem:{
+        flex:1,
+        margin:16,
+        height: 130,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset:{width:0, height:2},
+        shadowRadius: 8,
+        backgroundColor: 'white'
+    },
+    button:{
+        flex:1,
+    },
+    InnerContainer:{
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title:{
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+})
 
 export default CategoryGridTile;
