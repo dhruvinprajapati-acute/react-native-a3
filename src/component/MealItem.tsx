280d15327079c47640d7c7f0e0a83fec65bf696c
Dhruvin Prajapati <dhruvin.prajapati@acuteinformatics.in>
2024-08-30 11:17:35 +0530

    meal style update

--- src/component/MealItem.tsx
+++ src/component/MealItem.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
 function MealItem({title, imageUrl, duration, complexity, affordability}: any): React.JSX.Element {
     return(
-        <View>
-            <Pressable>
-                <View>
-                    <Image style={styles.image} source={{uri: imageUrl}} />
-                    <Text style={styles.title}>{title}</Text>
-                </View>
-                <View>
-                    <Text>{duration}m</Text>
-                    <Text>{complexity.toUpperCase()}</Text>
-                    <Text>{affordability.toUpperCase()}</Text>
+        <View style={styles.mealItem}>
+            <Pressable android_ripple={{color: '#ccc'}}
+                style={({pressed}) => [
+                    styles.button,
+                    pressed ? styles.buttonPressed : null,
+                    ]}>
+                <View style={styles.innerContainer}>
+                    <View>
+                        <Image style={styles.image} source={{uri: imageUrl}} />
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.detailItem}>{duration}m</Text>
+                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+                    </View>
                 </View>
             </Pressable>
         </View>
@@ -20,6 +26,27 @@ function MealItem({title, imageUrl, duration, complexity, affordability}: any): 
 }
 
 const styles = StyleSheet.create({
+    mealItem:{
+        margin: 16,
+        borderRadius: 8,
+        backgroundColor: 'white',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset:{width:0, height:2},
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    },
+    innerContainer:{
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    buttonPressed:{
+        opacity: 0.5,
+    },
+    button:{
+        flex:1,
+    },
     image:{
         width: '100%',
         height: 200,
@@ -28,6 +55,17 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         fontSize: 18,
+        margin: 8,
+    },
+    details:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 8,
+        justifyContent: 'center',
+    },
+    detailItem:{
+        marginHorizontal: 4,
+        fontSize: 12,
     },
 });
 export default MealItem;
